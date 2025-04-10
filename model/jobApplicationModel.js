8dd358fbcb38017f590d1bb24ed0c124c8359687
Techsoul437 <info@techsoulsolutions.com>
2025-04-10 09:41:46 +0530

    Job Application module changes

--- model/jobApplicationModel.js
+++ model/jobApplicationModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const jobApplicationSchema = new mongoose.Schema(
   {
@@ -18,16 +18,70 @@ const jobApplicationSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    position: {
+    resumeFile: {
       type: String,
       required: true,
     },
-    resumeFile: {
-      type: String, 
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Others"],
+      required: true,
+    },
+    totalExperience: {
+      type: String,
+      enum: [
+        "Fresher",
+        "1-2 Years",
+        "3-4 Years",
+        "5-6 Years",
+        "7-8 Years",
+        "9-10 Years",
+        "11-12 Years",
+        "13-14 Years",
+        "Over 15 Years",
+      ],
+    },
+    currentCTC: {
+      type: Number,
+    },
+    currentCTCRate: {
+      type: String,
+      enum: ["Per hour", "Per day", "Per week", "Per month", "Per year"],
+    },
+    expectedCTC: {
+      type: Number,
+    },
+    expectedCTCRate: {
+      type: String,
+      enum: ["Per hour", "Per day", "Per week", "Per month", "Per year"],
+    },
+    currentLocation: {
+      type: String,
+      required: true,
+    },
+    noticePeriod: {
+      type: String,
+      enum: [
+        "15 Days",
+        "30 Days",
+        "45 Days",
+        "60 Days",
+        "75 Days",
+        "90 Days",
+        "More than 90 Days",
+      ],
+    },
+    sourceOfReferral: {
+      type: String,
+      enum: ["Linkedin", "Facebook", "Instagram", "Twitter", "Other"],
+      required: true,
+    },
+    jobRole: {
+      type: String,
       required: true,
     },
   },
   { timestamps: true }
 );
 
-export default mongoose.model('JobApplication', jobApplicationSchema);
+export default mongoose.model("JobApplication", jobApplicationSchema);
